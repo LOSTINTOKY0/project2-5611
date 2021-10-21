5b75a188578b72860469f8cf56bfbc022a7b38ed
Audrey Hebert <heber169@umn.edu>
2021-10-21 18:46:42 -0500

    cleanup

--- Assets/ClothSim.cs
+++ Assets/ClothSim.cs
@@ -22,9 +22,9 @@ public class ClothSim : MonoBehaviour
   float friction = -.005f;
 
   //need to put positions of nodes into list by down-across rather than across-down
-  Vector3[] pos = new Vector3[nSlices*nStacks*4];
-  Vector3[] vel = new Vector3[nSlices*nStacks*4];
-  Vector3[] acc = new Vector3[nSlices*nStacks*4];
+  Vector3[] pos = new Vector3[nSlices*nStacks*2];
+  Vector3[] vel = new Vector3[nSlices*nStacks*2];
+  Vector3[] acc = new Vector3[nSlices*nStacks*2];
 
     List<Vector3> newVertices = new List<Vector3>();
     List<Vector2> newUV= new List<Vector2>();
@@ -43,8 +43,8 @@ public class ClothSim : MonoBehaviour
 
 public void Update(){
    //gravity gets reset each time
-  for(int j = 0; j< nSlices*2; j++){
-    for(int i = 0; i < nStacks*2; i++){
+  for(int j = 0; j< nSlices; j++){
+    for(int i = 0; i < nStacks; i++){
         //Debug.Log(j*nSlices + i);
         acc[j*nSlices + i] = gravity;
 
@@ -55,8 +55,8 @@ public void Update(){
 
 
   //Compute (damped) Hooke's law for each spring
-    for (int j = 0; j < nSlices*2; j++){
-  for (int i = 0; i < nStacks*2; i++){
+    for (int j = 0; j < nSlices; j++){
+  for (int i = 0; i < nStacks-1; i++){
     Vector3 diff = pos[nSlices*j + i+1]- pos[j*nSlices + i];
     float stringF = -k*(diff.magnitude - restLen);
     diff.Normalize();
@@ -72,13 +72,13 @@ public void Update(){
   }
     }
   //Eulerian integration
-    for (int j = 0; j < nSlices*2; j++){
-  for (int i = 1; i < nStacks*2; i++){
+    for (int j = 0; j < nSlices; j++){
+  for (int i = 1; i < nStacks; i++){
     vel[j*nSlices +i] = vel[j*nSlices +i]+(acc[j*nSlices +i]*(Time.deltaTime));
     pos[j*nSlices +i] = pos[j*nSlices +i]+(vel[j*nSlices +i]*(Time.deltaTime));
-    Debug.Log("delta time is " +Time.deltaTime);
+    //Debug.Log("delta time is " +Time.deltaTime);
 
-    Debug.Log("new pos is " + pos[j*nSlices +i]+(vel[j*nSlices +i]*(Time.deltaTime)/2.0f));
+  //  Debug.Log("new pos is " + pos[j*nSlices +i]+(vel[j*nSlices +i]*(Time.deltaTime)/2.0f));
   }
     }
     //updateMesh(GetComponent<MeshFilter>().mesh);
@@ -87,10 +87,9 @@ public void Update(){
 
 void ready(Mesh mesh){
   int a = 0;
-  for(float x = -width; x<width - 2.0f*width/nSlices; x+=(2.0f*width/nSlices)){
-      for(float y = -height; y < (height) ; y+=(height/nStacks)){
-        Debug.Log(x + " "+ y);
-          // float y2 = y+(2.0f*height/nStacks);
+  for(float x = 0; x<= width; x+=(width/(float)nSlices)){
+      for(float y = 0; y <= height ; y+=(height/(float)nStacks)){
+       Debug.Log(x + " "+ y);
            float x2 = x+(2.0f*(width/nSlices));
 
            //starts at 0,0 goes to 1,1 for image
@@ -103,20 +102,18 @@ void ready(Mesh mesh){
        //first 2 triangle points
            newVertices.Add( new Vector3(x,y,0));//1
            newVertices.Add(new Vector3(x2,y,0));//2
+           //Debug.Log("a is "+ a + " pos length is"+ pos.Length);
+           Debug.Log("vertices size is "+newVertices.Count);
            pos[a] = new Vector3(x,y,0);
            a++;
            pos[a] = new Vector3(x2,y,0);
            a++;
-      //add normals
-        //   normals.Add(Vector3(0,0,1).ToUnit());
-        //   normals.Add(Vector3(0,0,1).ToUnit());
-       //first 2 triangle image points
            newUV.Add(new Vector2(imgX,imgY));
            newUV.Add(new Vector2(imgX2,imgY));
 
 
            //now triangle indices
-           if(y!=-height){
+           if(y!=0){
            int vert = newVertices.Count-1;
                newTriangles.Add(vert);
                newTriangles.Add( vert-1);
@@ -136,90 +133,10 @@ void setupMesh(Mesh mesh){
   mesh.vertices = newVertices.ToArray();
     mesh.uv = newUV.ToArray();
     mesh.triangles = newTriangles.ToArray();
-    Debug.Log(mesh.vertices.Length + " " + nSlices*nStacks*4);
-
-    /*weights = new BoneWeight[newVertices.Count];
-    for (int i = 0; i<newVertices.Count; i++) {
-      weights[i].boneIndex0 = 0;
-
-      weights[i].weight0  = 0;
-
-    }
-    mesh.boneWeights = weights; */
+    Debug.Log(mesh.vertices.Length + " " + nSlices*nStacks);
 }
 
 void updateMesh(Mesh mesh){
   mesh.vertices = pos;
 }
-
-//how to setupMesh in 3D?
-//original code has nodes downwards,
-// x1, y1, then next would be x1 + width, y2
-//width*x + y
- //transfer proecsisng code to here!!!
-
-
-
-
-
-
-//processing code
-
-
-
-/*
-
-//Draw the scene: one sphere per mass, one line connecting each pair
-boolean paused = true;
-void draw() {
-  background(255,255,255);
-
-  //update(1/(20*frameRate))
-  if (!paused) {
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-update(1/(20*frameRate));
-
-
-
-
-};
-
-    for (int j = 0; j < strings; j++){
-  for (int i = 0; i < numNodes-1; i++){
-
-    pushMatrix();
-    line(pos.get(j)[i].x,pos.get(j)[i].y, pos.get(j)[i+1].z,pos.get(j)[i+1].x,pos.get(j)[i+1].y,pos.get(j)[i+1].z);
-    translate(pos.get(j)[i+1].x,pos.get(j)[i+1].y,pos.get(j)[i+1].z);
-    lights();
-    fill(230,90,230);
-    sphere(radius);
-    popMatrix();
-  }
-    }
-
-  }
-
-
-
-*/
-
-
 }
